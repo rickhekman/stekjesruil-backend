cdd25a8ca25374ea179322e14d7af4895c48fa38
rick hekman <rickhekman@gmail.com>
2021-03-19 11:53:57 +0100

    update packeges

--- database.js
+++ database.js
@@ -1,7 +1,8 @@
 const Sequelize = require('sequelize');
-const databaseUrl = process.env.DATABASE_URL;
+const databaseUrl = process.env.DATABASE_URL || 'postgres://postgres:cuttings@localhost:5432/postgres';
 const connection = new Sequelize(databaseUrl);
 
+console.log("PROCES ENV DATABASE",process.env.DATABASE_URL)
 connection
   .sync({ force: false })
   .then(() => console.log('Connected to database!'))
